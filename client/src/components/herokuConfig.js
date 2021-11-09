3bcbe86e3a03ea6b79f271218e06d88181bf9c08
CdoGGo <74845274+kcdoggo@users.noreply.github.com>
2021-11-09 18:52:36 +0900

    Update and rename config.js to herokuConfig.js

--- client/src/components/herokuConfig.js
+++ client/src/components/herokuConfig.js
@@ -1,6 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-
-  baseURL="https://cdoggo-platform.herokuapp.com/"
+    baseURL = "https://cdoggo-platform.herokuapp.com/api/"
 })
