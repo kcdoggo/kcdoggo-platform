e44cac34ee41798ff607d8e907db997052b639a3
CdoGGo <74845274+kcdoggo@users.noreply.github.com>
2021-11-09 21:48:16 +0900

    Update ImageSlider.js

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -8,7 +8,7 @@ function ImageSlider(props) {
                     {props.images.map((image,index)=>(
                         
                         <div key={index}>
-                            <img style={{width:'100%', maxHeight:'380px', objectFit:'contain'}} src={`http://localhost:5000/${image}`}/>
+                            <img style={{width:'100%', maxHeight:'380px', objectFit:'contain'}} src={`https://cdoggo-platform.herokuapp.com/${image}`}/>
                         
                         
                         
