483b252640edf0329ce292c89ebece2302d55282
CdoGGo <74845274+kcdoggo@users.noreply.github.com>
2021-11-09 19:02:33 +0900

    Update UploadProductPage.js

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,118 +1,99 @@
-import React, {useState} from 'react'
-import { Typography,Form, Input, Button } from 'antd';
-import FileUpload from '../../utils/FileUpload';
+import React,{useState} from 'react'
 import axios from 'axios';
-import "./UploadProductPageStyle.css";
+import { axiosInstance } from '../herokuConfig';
+import Dropzone from 'react-dropzone'
+//import {Icon} from 'antd';
+import { FaCloudUploadAlt } from "react-icons/fa";
 
+//여기props는 uploadProduct 부모컴포넌트
+function FileUpload(props) {
 
-const {TextArea} = Input;
-//option 태그 안에 들어가는 배열들...
-const Products = [{key:1, value:'Samsung'},{key:2,value:'Apple'}, {key:3, value:'LG'}, {key:4, value:'Huawei'}, {key:5,value:'Nokia'},{key:6,value:'Motorola'},{key:7,value:'Sony'},{key:8,value:'Google'},{key:9,value:'게임기기(닌텐도,xbox등)'}]
-
-
-
-function UploadProductPage(props) {
-    
-    
-    const [Title, setTitle] = useState("");
-    const [Description, setDescription] = useState("");
-    const [Price, setPrice] = useState("");
-    const [Product, setProduct] = useState(1) //option태그를 설정했을때 관리하는 Product의 state.
+    //백엔드product.js서 fileName,filePath보낼건데, 이걸 먼저 state에 저장
+    //이걸, 드롭존 아래에서 map으로images돌면서 이미지 출력할거.
+    //여기 Images에 filePath가 저장되있음. 
     const [Images, setImages] = useState([])
 
 
-    const titleChangeHandler = (event) => {
-        setTitle(event.currentTarget.value);
-    }
+    //파일을 백서버 노드에 전달.
+    const dropHandler = (files) => {
 
-    const descriptionChangeHandler = (event) =>{
-        setDescription(event.currentTarget.value);
-    }
+        //axios는 폼형식으로데이터못보내서,formData만듦
+        let formData = new FormData();
 
-    const priceChangeHandler = (event) => {
-        setPrice(event.currentTarget.value);
-    }
-
-    const productChangeHandler = (event) => {
-        setProduct(event.currentTarget.value)
-    }
-    //FileUpload의 state에 있는 images를 패러미터로 받아줄거임.
-    const updateImages = (newImages) =>{
-        setImages(newImages)
-    }
-
-    //서버에 데이터 전송기능(타이틀,설명,가격등)
-    const submitHandler = (event) =>{
-        event.preventDefault();//페이지 리프레시 방지.
-        //유효성 체크,모든 칸이 채워지지 않으면 submit못하게
-        if(!Title || !Description || !Price || !Product || !Images.length === 0){
-           return alert("모든 칸을 기재바랍니다");
+        //append로 키:값형식으로추가. formData에 보낼데이터 저장됨.
+        formData.append("file",files[0]) 
+       
+        //어떤데이터형식보내는지서버에알려줘야함.
+        const config ={
+            header: {'content-type':'multipart/form-data'}
         }
+       
 
-        //보낼데이터 객체만듦, auth.js의 유저 prop을 받아옴.
-        //필드들은 당연히 db모델 product.js따라가지.
-        const body={
-            writer: props.user.userData._id,
-            title:Title,
-            description:Description,
-            price:Price,
-            images:Images,
-            products:Product
 
-        }
-        //서버에 채운값들을 request로 보내기,axio(주소,보낼데이터객체)
-        axios.post("/api/product",body)
-            .then(response=>{//백엔드에서 처리한 후에,response보낼거.
+
+        //axios사용해 파일 전달.보낼서버주소,보낼데이터,보낼형식
+        axiosInstance.post('/api/product/image',formData,config)
+            .then(response=>{//서버에서 정보전달해주는거then(response)으로 받음.
                 if(response.data.success){
-                    alert('상품 업로드 되었습니다.')
-                    props.history.push('/landing')
+                    console.log(response.data)
+
+                    //원래 있었던 이미지를 spread연산자 이용해서 넣어줌., response.data새로들어온데이터
+                    setImages([...Images,response.data.filePath])
+
+                    //uploadProductPage의 prop를 받아옴.이미지를 부모컴포넌트(업로드프로덕트)에 넘김
+                    props.refreshFunction([...Images,response.data.filePath])
+
                 }else{
-                    alert('상품 업로드를 하지 못했습니다. 문의바랍니다.')
+                    alert('파일 저장 실패')
                 }
             }) 
-
     }
+
+    const deleteHandler = (image) =>{
+
+        //이미지에대한 인덱스 알기위해
+        const currentIndex = Images.indexOf(image)
+        let newImages = [...Images] //현이미지 다 복사해서 새로운 배열 만들어줌.
+        newImages.splice(currentIndex,1);//splice함수로,현재인덱스넘버에서,하나의아이템삭제.
+        setImages(newImages)
+        
+        //삭제했을때도 서버에 알려줘야하니, 부모컴포넌트 prop으로 넘김(uploadProduct)
+        props.refreshFunction(newImages)
+    
+    }
+
+
     return (
-        <div className="uploadForm" >
-        <div style={{backgroundColor:"white" ,maxWidth:'700px',margin:"0 auto",padding:'2rem 2rem',borderRadius:'20px',boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
-            <div style={{textAlign:'center',marginBottom:'2rem'}}>
-                <h1 style={{fontWeight:'900'}}>디지털 상품 업로드</h1>
-            </div>
+        <div style={{display:'flex', justifyContent:'space-between' }}>
+           <Dropzone onDrop={dropHandler}>
+                {({getRootProps, getInputProps}) => (
+                    <div 
+                    style={{width:300,height:240, border: '1px solid lightgrey',display:'flex', justifyContent:'center',alignItems:'center'   }}
+                     {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <FaCloudUploadAlt style={{fontSize:'8rem'}}/>
+                        {/*<Icon type="plus" style={{fontSize:'3rem'}}/>*/}
 
-            <Form onSubmit={submitHandler}>
-                {/** FileUpload에 있는 이미지들을 이 부모컴포넌트로 가져와야하는게 목적.
-                 prop을 refreshFuction으로주고,펑션을 줌. 그럼 이 prop을 fileupload에 적어줘야지.*/}
-                <FileUpload refreshFunction={updateImages}/>
-                <div style={{display:'flex', justifyContent:'flex-end',maxWidth:'700px',color:'lightgrey',paddingTop:'10px'}}>(*)이미지를 클릭하여 삭제하세요</div>
-
-
-                <br/>
-                <br/>
-                <label>제품명</label>
-                <Input onChange={titleChangeHandler} value={Title}/>
-                <br/>
-                <br/>
-                <label>설명</label>
-                <TextArea onChange={descriptionChangeHandler} value={Description}/>
-                <br/>
-                <br/>
-                <label>가격(원)</label>
-                <Input type="number" onChange={priceChangeHandler} value={Price}/>
-                <br/>
-                <br/>
-                <select onChange={productChangeHandler} value={Product}>
-                    {Products.map(item => (
-                        <option key={item.key} value={item.key}>{item.value}</option>
+                    </div>
+            )}
+            </Dropzone>
+                        {/**dropzone옆 저장된사진 띄우는곳. */}
+               <div style={{display:'flex', width:'350px', height:'240px',overflowX:'scroll'}}>
+                  
+                    {Images.map((image,index)=>(
+                        //deleteHandler함수호출후,onClick에 이 함수를 지정해줌,바로 onClick={deleteHandler}해주면,이것의리턴값을onClick에지정함.
+                        <div onClick={()=>deleteHandler(image)} key={index}>
+                            <img style={{minWidth:'300px', width:'300px',height:'240px' }}
+                            src={`http://localhost:5000/${image}`} //노드 index.jsp에서 static파일로 upload지정해줘서 거기있는 사진 출력. 
+                            />
+                        </div>    
                     ))}
-                </select>
-                <br/>
-                <br/> {/**andt form 코드에,submit은 htmlType="submit"으로정의해야함.type으로 하면 안넘어가. */}
-                <Button htmlType="submit" style={{width:'640px', backgroundColor:'#2B56D8',fontWeight:'900',padding:'19px 2px', color:'white',fontSize:'20px',display:'flex',alignItems:'center',justifyContent:'center',borderRadius:'3px'}}>SUBMIT</Button>
-            </Form>
 
-        </div>
+
+               </div>     
+
         </div>
     )
 }
 
-export default UploadProductPage
+export default FileUpload
