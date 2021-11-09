b3e4abf5a798ba2bb723ca014ba0dd237f5d6566
CdoGGo <74845274+kcdoggo@users.noreply.github.com>
2021-11-09 21:57:59 +0900

    Update ProductImage.js

--- client/src/components/views/DetailProductPage/Sections/ProductImage.js
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -12,8 +12,8 @@ function ProductImage(props) {
 
         props.detail.images.map(item => {
             images.push({
-                original: `http://localhost:5000/${item}`,
-                thumbnail: `http://localhost:5000/${item}`,
+                original: `https://cdoggo-platform.herokuapp.com/${item}`,
+                thumbnail: `https://cdoggo-platform.herokuapp.com/${item}`,
                 originalHeight: '500px',
                 originalWidth:'700px'
             })
