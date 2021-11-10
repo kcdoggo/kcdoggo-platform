d2d873ffc3d3ddaf832ce75ddcd8c902ea45a25e
CdoGGo <74845274+kcdoggo@users.noreply.github.com>
2021-11-10 20:42:55 +0900

    Update FirstSection.js

--- client/src/components/views/Intro/FirstSection.js
+++ client/src/components/views/Intro/FirstSection.js
@@ -8,8 +8,8 @@ export default () => {
 
   return (
     <div className="component first-component">
-        <div className="font-link"style={{fontSize:'3rem',color:'black',
-        display:'flex',alignItems:'center',justifyContent:'center',width:'100vw',height:'100vh',marginTop:'-20px',textAlign:'center'}}>
+        <div className="font-link"style={{fontSize:'3rem',color:'black', textAlign:'center',
+        display:'flex',alignItems:'center',justifyContent:'center',width:'100vw',height:'100vh',marginTop:'-20px'}}>
         <Typewriter
              options={{
                 strings: ['CdoGGo에 오신걸 환영합니다','즐거운 쇼핑 되세요 !' ],
