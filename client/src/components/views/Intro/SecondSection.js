1444f96beb3b7956f56c8591e040a0d34c98c393
CdoGGo <74845274+kcdoggo@users.noreply.github.com>
2021-11-15 11:40:55 +0900

    Update SecondSection.js

--- client/src/components/views/Intro/SecondSection.js
+++ client/src/components/views/Intro/SecondSection.js
@@ -8,7 +8,12 @@ export default () => {
   return (
     <div className="component second-component" >
       <div className="font-link" style={{color:'black'}}>
-       .         
+       <div className="main-text1">
+          <div>핸드폰을 팔려고 보니</div>
+          <div>100만원에 산 내 핸드폰이</div>
+          <div style={{color:'white'}}>20만원대로 ?</div>
+       </div>
+
       </div>
     </div>
   );
