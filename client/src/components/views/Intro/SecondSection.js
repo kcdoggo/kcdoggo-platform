9d0151873ee19e1c7ac48b407f79c0462210ed67
CdoGGo <74845274+kcdoggo@users.noreply.github.com>
2021-11-15 15:20:51 +0900

    Update SecondSection.js

--- client/src/components/views/Intro/SecondSection.js
+++ client/src/components/views/Intro/SecondSection.js
@@ -24,11 +24,7 @@ export default () => {
           <div>100만원에 산 내 핸드폰이</div>
           <div style={{color:'white'}}>20만원대로 ?</div>
        </div>
-        <img src={questionBox}>
-         <div>기능차감, 구성품 차감</div>
-         <div>액정차감, 외관 차감</div>
-         <div>왜 이렇게 차감이 많아?</div>
-       </img>
+       
        
        <img src={perplexed}/>
    
