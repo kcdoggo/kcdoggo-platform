b5f74ede07e9e33f69c527a6961c36344509be6c
CdoGGo <74845274+kcdoggo@users.noreply.github.com>
2021-11-17 09:59:12 +0900

    Update ThirdSection.js

--- client/src/components/views/Intro/ThirdSection.js
+++ client/src/components/views/Intro/ThirdSection.js
@@ -5,6 +5,8 @@ import { Col,Row } from "antd";
 export default () => {
   
   document.body.style.overflow = "hidden"
+  document.body.style.width = "100%";
+document.body.style.height = "100%";
 
   return (
     <div className="Icomponent Ithird-component" >
